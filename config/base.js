e0b6accf3131ada2b4ab8494f7aa15303d25802b
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-06-10 12:40:08 -0400

    debugging environment variables not working

--- config/base.js
+++ config/base.js
@@ -1,4 +1,4 @@
-console.log("what environtment?: ", process.env.NODE_ENV)
+console.log("secret key?: ", process.env.SECRET_KEY)
 switch (process.env.NODE_ENV) {
   case 'staging':
     module.exports = require('./stag')
