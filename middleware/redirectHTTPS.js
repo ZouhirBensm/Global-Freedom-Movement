b7da72e4cc339cbfbca0cf1698334cee3b503bcb
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-06-10 17:33:20 -0400

    working on redirecting requests to https always

--- middleware/redirectHTTPS.js
+++ middleware/redirectHTTPS.js
@@ -1,14 +1,14 @@
 const ENV = require('../config/config')
 
 module.exports = (req,res,next)=>{
-  if(ENV.environment === 'staging'){
+  if(ENV.environment === 'staging' && req.method==="GET"){
     console.log("what is the Fuckin protocol: ", req.header('x-forwarded-proto'))
     console.log(`https://${req.header('host')}${req.url}`)
     // next()
-    // if (req.header('x-forwarded-proto') !== 'https')
-    //   res.redirect(`https://${req.header('host')}${req.url}`)
-    // else
-    //   next()
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
   }
   next()
 }
