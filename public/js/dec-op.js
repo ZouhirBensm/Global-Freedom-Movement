5fe42df59143b817ddbb67baf90f876fdd18acd6
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-06-20 18:32:30 -0400

    changed to op page to function on hostgator

--- public/js/dec-op.js
+++ public/js/dec-op.js
@@ -2,39 +2,40 @@ console.log(document.readyState)
 
 if(environment != "developement"){
   document.getElementById("decy-id").remove()
-}
-
-document.getElementById("local-dec").addEventListener("click", async (e) => {
-  console.log("local click")
-  console.log(`${endpoint}/localhost-dec`)
-  
-  try {
-    response = await fetch(`${endpoint}/localhost-dec`, {
-      method: 'GET',
-    })
-  } catch (e) {
-    console.error(`Catched Error: ${e}`)
-  }
-  // console.log(response)
-  let json_response
-  try {json_response = await response.json()} catch(e) {console.error(e)}
-  console.log("server response: ", json_response)
-  
-  if(response && json_response){
-    if(response.ok) {
-      
-      
+} else {
+  document.getElementById("local-dec").addEventListener("click", async (e) => {
+    console.log("local click")
+    console.log(`${endpoint}/localhost-dec`)
+    
+    try {
+      response = await fetch(`${endpoint}/localhost-dec`, {
+        method: 'GET',
+      })
+    } catch (e) {
+      console.error(`Catched Error: ${e}`)
+    }
+    // console.log(response)
+    let json_response
+    try {json_response = await response.json()} catch(e) {console.error(e)}
+    console.log("server response: ", json_response)
+    
+    if(response && json_response){
+      if(response.ok) {
+        
+        
+      } else {
+        
+        
+      }
     } else {
       
       
     }
-  } else {
-    
-    
-  }
+  
+  })
+  
+  document.getElementById("staging-hostgator-dec").addEventListener("click", (e) => {
+    console.log("remote click")
+  })
+}
 
-})
-
-document.getElementById("staging-hostgator-dec").addEventListener("click", (e) => {
-  console.log("remote click")
-})
